fca5cc73636568204f98094aaf5c3d70cd853ed4
correaa1 <bruno_correa_ds@hotmail.com>
2020-10-24 23:53:41 -0300

    Final

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
+import {Picker} from '@react-native-community/picker';
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      pizza: 0,
+      pizzas: [
+        {key: 1, nome: 'Calabresa', valor: 20.9},
+        {key: 2, nome: 'Mussarela', valor: 19.9},
+        {key: 3, nome: 'Portuguesa', valor: 21.9},
+        {key: 4, nome: 'Quatro Queijo', valor: 22.9},
+        {key: 5, nome: 'Marguerita', valor: 22.9},
+      ],
+    };
   }
-
   render() {
+    let pizzasItem = this.state.pizzas.map((v, k) => {
+      return <Picker.Item key={k} value={v} label={v.nome} />;
+    });
+
     return (
-      <View>
-        <Text> App </Text>
+      <View style={styles.container}>
+        <Text style={styles.logo}> Menu Pizza</Text>
+
+        <Picker
+          selectedValue={this.state.pizza}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({pizza: itemValue})
+          }>
+          {pizzasItem}
+        </Picker>
+
+        <Text style={styles.pizzas}>
+          Voce escolheu:{this.state.pizzas[this.state.pizza].nome}
+        </Text>
+        <Text style={styles.pizzas}>
+          R$: {this.state.pizzas[this.state.pizza].valor.toFixed(2)}
+        </Text>
       </View>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  logo: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+  },
+  pizzas: {
+    marginTop: 15,
+    fontSize: 25,
+    textAlign: 'center',
+  },
+});
